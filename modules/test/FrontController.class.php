1d553ef257c087646c1cca056c7b75e4788d1da9
Maxime <max1789@hotmail.com>
2012-10-26 01:12:38 +0200

    Problème avec le module test.

--- modules/test/FrontController.class.php
+++ modules/test/FrontController.class.php
@@ -4,12 +4,12 @@ class FrontController extends \SWAF\Core\Controller
 {
     public function home ()
     {
-        echo "<h1>TEST</h1>";
+        echo "<h1>Test</h1>";
     }
 
     public function test ($test)
     {
-        echo "<h1>TEST</h1>$test";
+        echo "<h1>Test</h1>$test";
     }
 }
 
